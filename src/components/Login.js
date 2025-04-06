829ce2607e47963c0b15ae11bac0946cbfb6eeb4
BrandenHicks <brandenrhicks@icloud.com>
2025-04-06 13:36:32 -0400

    removed the frontend folder, react router, and added placeholders for the web page.

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Login Page</h1>
+      <p>This is where your Microsoft Entra login or guest login would go.</p>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Quiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Quiz() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Quiz Page</h1>
+      <p>Trivia questions would be shown here!</p>
+    </div>
+  );
+}
+
+export default Quiz;
